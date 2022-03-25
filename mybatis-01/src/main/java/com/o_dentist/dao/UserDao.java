ce9143b4f9d05fb80c17a46020c4a2d211c74eb2
O.Dentist <1983510227@QQ.COM>
2022-03-25 22:57:24 +0800

    DEMO2_TEST

--- mybatis-01/src/main/java/com/o_dentist/dao/UserDao.java
+++ mybatis-01/src/main/java/com/o_dentist/dao/UserDao.java
@@ -2,8 +2,10 @@ package com.o_dentist.dao;
 
 import com.o_dentist.pojo.User;
 
+import javax.jnlp.ClipboardService;
 import java.util.List;
 
 public interface UserDao {
     public List<User> getUserList();
+    public List<User> addUser(int id,String name,String pwd);
 }
--- mybatis-01/src/test/java/com/o_dentist/dao/mybatisTest.java
+++ mybatis-01/src/test/java/com/o_dentist/dao/mybatisTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-public class UserDaoTest {
+public class mybatisTest {
     @Test
     public void test(){
 

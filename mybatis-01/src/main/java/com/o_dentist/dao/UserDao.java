422c4836ca492809f39f1e39ad727bb7c6a0cfcf
O.Dentist <1983510227@QQ.COM>
2022-03-28 16:28:44 +0800

    DEMO2_TEST

--- mybatis-01/src/main/java/com/o_dentist/dao/UserDao.java
+++ mybatis-01/src/main/java/com/o_dentist/dao/UserDao.java
@@ -1,11 +1,14 @@
 package com.o_dentist.dao;
 
 import com.o_dentist.pojo.User;
+import org.apache.ibatis.annotations.Param;
 
 import javax.jnlp.ClipboardService;
 import java.util.List;
 
 public interface UserDao {
     public List<User> getUserList();
-    public List<User> addUser(int id,String name,String pwd);
+    public void addUser(User user);
+    public void deleteUser(String name);
+    public void updateUser(User user);
 }
--- mybatis-01/src/test/java/com/o_dentist/dao/mybatisTest.java
+++ mybatis-01/src/test/java/com/o_dentist/dao/mybatisTest.java
@@ -9,9 +9,9 @@ import java.util.List;
 
 public class mybatisTest {
     @Test
-    public void test(){
+    public void test() {
 
-        //1.获取SqlSession对象
+        //1.获取SqlSession对象-getconnection()
         SqlSession sqlSession = MybatisUtils.getSqlSession();
         //2.执行SQL
         // 方式一：getMapper
@@ -24,4 +24,20 @@ public class mybatisTest {
         //关闭sqlSession
         sqlSession.close();
     }
+
+    @Test
+    public void Insert() {
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserDao mapper = sqlSession.getMapper(UserDao.class);
+//        User user = new User(4, "赵四", "12306");
+//        try {
+//            mapper.addUser(user);
+//            sqlSession.commit();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+        sqlSession.close();
+    }
 }
+
+
